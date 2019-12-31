bab4c1c83541eb5106900745f7460929273f3c3c
Ilya Petrov <dnamix1@gmail.com>
2019-12-31 11:07:16 +0300

    avl-tree rotation

---
+++ avl-tree.js
@@ -0,0 +1,43 @@
+class Tree {
+  constructor() {
+    this.root = null;
+  }
+  add(value) {
+    if (!this.root) {
+      this.root = new Node(value);
+    } else {
+      this.root.add(value);
+    }
+  }
+}
+
+class Node {
+  constructor(value, left = null, right = null) {
+    this.value = value;
+    this.left = left;
+    this.right = right;
+    this.height = 1;
+  }
+  add(value) {
+    if (value < this.value) {
+      // go left
+      if (this.left) {
+        this.left.add(value);
+      } else {
+        this.left = new Node(value);
+      }
+      if (!this.right || this.right.height < this.left.height) {
+        this.height = this.left.height + 1;
+      }
+    } else {
+      if (this.right) {
+        this.right.add(value);
+      } else {
+        this.right = new Node(value);
+      }
+      if (!this.left || this.right.height > this.left.height) {
+        this.height = this.right.height + 1;
+      }
+    }
+  }
+}
